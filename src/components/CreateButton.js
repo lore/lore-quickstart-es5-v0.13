f93dea17219e72f248277a48cde5e7f0a84206c9
Jason Hansen <jason_hansen@outlook.com>
2018-06-23 23:15:19 -0700

    [optimistic] step 4, add optimistic values

--- src/components/CreateButton.js
+++ src/components/CreateButton.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import createReactClass from 'create-react-class';
 import PropTypes from 'prop-types';
-import CreateTweetDialog from './CreateTweetDialog';
+import _ from 'lodash';
 
 export default createReactClass({
   displayName: 'CreateButton',
 
+  contextTypes: {
+    user: PropTypes.object.isRequired
+  },
+
   onClick() {
+    const { user } = this.context;
+
     lore.dialog.show(function() {
       return lore.dialogs.tweet.create({
         blueprint: 'optimistic',
         request: function(data) {
-          return lore.actions.tweet.create(data).payload;
+          return lore.actions.tweet.create(_.defaults({
+            user: user.id,
+            createdAt: new Date().toISOString()
+          }, data)).payload;
         }
       });
     });
