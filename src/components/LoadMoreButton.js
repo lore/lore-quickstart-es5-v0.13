cd91223ea77825d7df9d0764c1be2560ac6d0dfd
Jason Hansen <jason_hansen@outlook.com>
2018-06-23 22:17:08 -0700

    [infinite-scrolling] step 1, create load more button

---
+++ src/components/LoadMoreButton.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import createReactClass from 'create-react-class';
+import PropTypes from 'prop-types';
+import PayloadStates from '../constants/PayloadStates';
+
+export default createReactClass({
+  displayName: 'LoadMoreButton',
+
+  propTypes: {
+    lastPage: PropTypes.object.isRequired,
+    onLoadMore: PropTypes.func.isRequired,
+    nextPageMetaField: PropTypes.string.isRequired
+  },
+
+  render() {
+    const {
+      lastPage,
+      onLoadMore,
+      nextPageMetaField
+    } = this.props;
+
+    if(lastPage.state === PayloadStates.FETCHING) {
+      return (
+        <div className="footer">
+          <div className="loader"/>
+        </div>
+      );
+    }
+
+    if (!lastPage.meta[nextPageMetaField]) {
+      return (
+        <div className="footer"/>
+      );
+    }
+
+    return (
+      <div className="footer">
+        <button className="btn btn-default btn-lg" onClick={onLoadMore}>
+          Load More
+        </button>
+      </div>
+    );
+  }
+
+});
