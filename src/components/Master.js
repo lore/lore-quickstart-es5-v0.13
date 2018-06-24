019df3181662d195672ed006fe17089f481a7aae
Jason Hansen <jason_hansen@outlook.com>
2018-06-24 00:14:11 -0700

    [websockets] step 1, add websocket support

--- src/components/Master.js
+++ src/components/Master.js
@@ -39,6 +39,15 @@ createReactClass({
     };
   },
 
+  componentDidMount() {
+    lore.websockets.tweet.connect();
+    lore.websockets.tweet.subscribe();
+  },
+
+  componentWillUnmount() {
+    lore.websockets.tweet.unsubscribe();
+  },
+
   render() {
     const { user } = this.props;
 
